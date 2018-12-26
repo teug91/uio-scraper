260a5566ec054941b31d953964b30e9282fcdd2b
Terje Uglebakken <teug91@gmail.com>
2018-12-27 00:11:20 +0100

    Ignores links which are not files. Command arg. Packaging.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # uio-scraper
+
+Tool for downloading pdf's and other files from UIO course pages. Example:
+```
+uioscraper https://www.uio.no/studier/emner/matnat/ifi/IN5320/h18/assignments/
+```
--- uioscraper.py
+++ uioscraper.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
-import httplib2, re, wget, os
+import httplib2, re, wget, os, sys
 from bs4 import BeautifulSoup
 
-url = input('Enter URL:\n')
+url = sys.argv[1]
 
 headers, body = httplib2.Http().request(url)
 soup = BeautifulSoup(body, 'html.parser')
-for link in soup.find_all('a', href=re.compile("^{}".format(url))):
-    wget.download(link.get('href'), out=os.path.join(os.path.expanduser('~'), 'Downloads/'))
+paths = []
+
+for link in soup.find_all('a', href=re.compile('^{}.+\\..+'.format(url))):
+    path = link.get('href')
+    if path not in paths:
+        paths.append(path)
+        wget.download(path, out=os.path.join(os.path.expanduser('~'), 'Downloads/'))
+
+print('\n{} files found.'.format(len(paths)))
